496879acb8376fabb6755eea2bc9bae5fb56ab73
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-05-28 23:38:24 +0300

    MM

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,8 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
 	const obj = {
-		[departmentName] : [
+		[departmentName] :
           		employees,
-		],
   	};
 	return obj
 }
