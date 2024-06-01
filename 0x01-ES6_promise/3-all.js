2c4fa9a9cdb6ece221e4b01c075b6cd26fa980ea
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-06-01 17:43:18 +0300

    MM

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,5 +6,4 @@ export default function handleProfileSignup() {
        console.log( res[1].body ,res[0].firstName ,res[0].lastName );
      })
      .catch(() => console.log("Signup system offline"));
-
 }
