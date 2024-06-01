5b8ddaaac38f98b006401d8253bc99acd4e059ac
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-06-01 16:18:31 +0300

    MM

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  console.log('Got a response from the API');
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error());
-    }
-  });
+    return promise
+      .then(() => ({
+        status: 200, 
+	body: 'Success', 
+      }))
+      .catch(() => Error())
+      .finally(() => console.log('Got a response from the API'));
 }
