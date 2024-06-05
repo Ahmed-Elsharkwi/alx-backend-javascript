d96dd4cf7c709189d9fba6f35b5528ce6fe7930d
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-06-05 18:12:06 +0300

    MM

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const map_1 = new Map()
+  map_1.set('Apples', 10)
+  map_1.set('Tomatoes', 10)
+  map_1.set('Pasta', 1)
+  map_1.set('Rice', 1)
+  map_1.set('Banana', 5)
+  return map_1
+}
