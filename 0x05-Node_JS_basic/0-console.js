e0eb8d558b995d248336c137e736f6cadf0ea263
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-07-23 17:16:39 +0300

    MM

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,8 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
 function displayMessage(string) {
  console.log(string);
 }
