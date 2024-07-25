4718398c3e6d39c9a7da0bb8a217c49f911919b4
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-07-25 15:50:55 +0300

    MM

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,20 +1,20 @@
-const sum = require("./0-calcul.js");
+const calculateNumber = require("./0-calcul.js");
 const assert = require("assert")
 
 var result = 0;
 
-result = sum(4.5, 5.5);
+result = calculateNumber(4.5, 5.5);
 assert( result == 11)
 
-result = sum(-1.5, -2.5);
+result = calculateNumber(-1.5, -2.5);
 assert( result == -3);
 
-result = sum(9, 5);
+result = calculateNumber(9, 5);
 assert( result == 14)
 
-result = sum(1.5, 5)
+result = calculateNumber(1.5, 5)
 assert ( result == 7)
 
 
-result = sum(2, 5.5)
+result = calculateNumber(2, 5.5)
 assert (result == 8)
