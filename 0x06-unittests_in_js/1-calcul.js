841ec3a56f8d0fba3446dbe5344c2acef4c046de
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-07-25 16:37:14 +0300

    MM

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,17 @@
+const calculateNumber = (a, b, type) => {
+  a = Math.round(a);
+  b = Math.round(b);
+  if (type == "SUM") {
+     return (a + b);
+  }
+  else if (type == "SUBTRACT") {
+     return (b - a)
+  }
+  else if (type == "DIVIDE") {
+     if (b == 0) {
+        return "Error"
+     }
+     return (a / b);
+  }
+}
+module.exports = calculateNumber
---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,36 @@
+const calculateNumber = require("./1-calcul.js");
+const assert = require("assert")
+
+var result = 0;
+
+describe("calculateNumber", function() {
+  it("checks equality", function() {
+    assert.equal(calculateNumber(4.5, 5.5, "SUM"), 11);
+  });
+
+  it("checks equality", function() {
+    assert.equal(calculateNumber(-2.5, -3.5, "SUM"), -5);
+  });
+
+  it("checks equality", function() {
+    assert.equal(calculateNumber(1.0, 5.5, "SUBTRACT"), 5);
+  });
+
+  it("checks equality", function() {
+    assert.equal(calculateNumber(-4.5, 5.0, "SUBTRACT"), 9);
+  });
+
+  it("checks equality", function() {
+    assert.equal(calculateNumber(10, 5, "DIVIDE"), 2);
+  });
+
+  it("checks equality", function() {
+   assert.equal(calculateNumber(1.0, 0.4, "DIVIDE"), "Error");
+  });
+
+
+  it("checks equality", function() {
+   assert.equal(calculateNumber(4.4999999, 0.5, "DIVIDE"), 4);
+  });
+
+});
